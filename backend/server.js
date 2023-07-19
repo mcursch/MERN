f2dde4b3a15933d722eeb0c5cc22ee66d56683a6
matt curschman <mattcurschman@gmail.com>
2023-07-18 20:13:39 -0400

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
-import express from "express"
-import cors from "cors"
-import restaurants from "./api/restaruaunts.route.js"
+// import express from "express"
+// import cors from "cors"
+// import restaurants from "./api/restaurants.route.js"
 
-//create app with express
-const app = express()
-//use cors for proper routing
-app.use(cors())
-app.use(express.json())
+// //create app with express
+// const app = express()
+// //use cors for proper routing
+// app.use(cors())
+// app.use(express.json())
 
-//setup first url route
-app.use("/api/v1/restaraunts", restaraunts)
-//add exception for non specified routes
-app.use("*", (req, res) => res.status(404).json({error: "not found"}))
+// //setup first url route
+// app.use("/api/v1/restaraunts", restaurants)
+// //add exception for non specified routes
+// app.use("*", (req, res) => res.status(404).json({error: "not found"}))
 
-//export this to be imported in run file
-export default app
+// //export this to be imported in run file
+// export default app
+
+
+console.log('Hello World')
